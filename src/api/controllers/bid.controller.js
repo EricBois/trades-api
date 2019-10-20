eb04a49ce6eccd68eaad790d5b88aa393b8ed87a
Eric Boisvenu <burn4live@gmail.com>
2019-10-20 14:09:06 -0600

    removed management

--- src/api/controllers/bid.controller.js
+++ src/api/controllers/bid.controller.js
@@ -1,21 +1,8 @@
 const Bid = require('../models/bid.model');
-var ManagementClient = require('auth0').ManagementClient;
-
-var auth0 = new ManagementClient({
-  domain: 'dev-2upadx1s.auth0.com',
-  clientId: `${process.env.AUTH0_MANAGEMENT_ID}`,
-  clientSecret: `${process.env.AUTH0_MANAGEMENT_SECRET}`,
-  scope: 'read:users update:users'
-});
 
 exports.create = async (req, res, next) => {
   try {
     req.body.user = req.user.sub;
-    if (!req.user.phone || !req.user.email) {
-      const user = await auth0.getUser({ id: req.user.sub });
-      req.body.phone = user.user_metadata.phone
-      req.body.email = user.email
-    }
     // const account = await Account.findOne({ user: req.user.sub });
     // req.body.createdBy = account.name;
     const bid = await (new Bid(req.body)).save();
